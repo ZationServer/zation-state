fecde1892d38f1a26d720a7d7861cbeeb775e752
Luca Scaringella <lucalstyle@gmail.com>
2021-08-20 17:26:51 +0200

    Implement a new state run file.

---
+++ src/run.ts
@@ -0,0 +1,19 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+import {StateServer,LogLevel} from "ziron-state";
+import {secrets} from "docker-secret";
+
+const variables = Object.assign({}, process.env, secrets);
+
+process.title = `Zation State`;
+new StateServer({
+    secret: variables.SECRET,
+    port: parseInt(variables.PORT) || 7777,
+    path: variables.PATH || "/",
+    logLevel: parseInt(variables.LOG_LEVEL) || LogLevel.Everything,
+    scaleDelay: parseInt(variables.SCALE_DELAY) || 100
+}).listen();
